9d2f866168662cc806810e877ddb9c9657045387
Heba89-lj <h.jojo.loly.89@gmail.com>
2025-10-14 00:37:03 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,8 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("qr-app-v1").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "https://unpkg.com/qrcodejs@1.0.0/qrcode.min.js",
-        "https://unpkg.com/qr-scanner@1.4.2/qr-scanner.umd.min.js"
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+  console.log('Service Worker installed');
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', event => {
+  // يسمح للصفحة بالعمل حتى بدون إنترنت جزئياً
 });
-
 
